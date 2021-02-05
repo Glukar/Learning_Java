be92f5cc2962c56f87df6d81fb9e8b5a62e3d4a9
Glukar <70852474+Glukar@users.noreply.github.com>
2021-02-05 10:19:18 +0100

    Add files via upload

---
+++ Vezbe/NizoviVezba.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class NizoviVezba {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    // Deklaracija
+    int duz1, duz2, i, j;
+    boolean nadjen;
+    System.out.print("Unesite duzinu prvog niza: ");
+    duz1 = input.nextInt();
+    int[] niz1 = new int[duz1];
+    for (i = 0; i < niz1.length; i++) {
+      System.out.print("Unesite " + i + ". element prvog niza: ");
+      niz1[i] = input.nextInt();
+    }
+    System.out.print("Unesite duzinu drugog niza: ");
+    duz2 = input.nextInt();
+    int[] niz2 = new int[duz2];
+    for (i = 0; i < niz2.length; i++) {
+      System.out.print("Unesite " + i + ". element drugog niza: ");
+      niz2[i] = input.nextInt();
+    }
+   System.out.print("Sledeci elementi se nalaze u oba niza: ");
+   for (i = 0;i < duz1; i++) {
+     j = 0;
+     nadjen = false;
+     while (j < duz2 && !nadjen) {
+       if (niz1[i] == niz2[j]) {
+         nadjen = true;
+         System.out.print(" " + niz1[i]);
+       } else {
+         j++;
+       }
+     }
+   }
+    }
+  }
